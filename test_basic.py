c1f03ab472db8ca4397bd4be26df67e655b982a3
Dhrumil Mehta <dhrumil.mehta@columbia.edu>
2025-04-12 00:10:50 -0400

    update basic

--- test_basic.py
+++ test_basic.py
@@ -1,13 +1,16 @@
-from langchain_openai import ChatOpenAI
-from browser_use import Agent
 import asyncio
+from browser_use import Agent
+from langchain_openai import ChatOpenAI
+
+# load OPENAI_API_KEY from .env file
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 async def main():
     agent = Agent(
         task="Get me the top 5 posts on hacker news",
-        llm=ChatOpenAI(model="gpt-4o"),
+        llm=ChatOpenAI(model="gpt-4o", api_key=os.getenv('OPENAI_API_KEY')),
     )
     await agent.run()
 
